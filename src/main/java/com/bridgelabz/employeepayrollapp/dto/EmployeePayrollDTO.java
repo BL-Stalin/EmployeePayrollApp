e07144f00f6809f48a72153a60d09ef4c163d957
stalin-21 <stalinguna21@gmail.com>
2026-01-22 10:51:23 +0530

    Day6 Section3 UC1: added Lombok and removed boilerplate code

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,22 +1,9 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-public class EmployeePayrollDTO {
+import lombok.Data;
 
+@Data
+public class EmployeePayrollDTO {
     private String name;
     private long salary;
-
-    public EmployeePayrollDTO() {}
-
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
 }
