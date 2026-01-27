976c7595b62ab4bad4aacdbae33857ca52a8123b
stalin-21 <stalinguna21@gmail.com>
2026-01-27 12:18:09 +0530

    Day7 UC6: added custom query to fetch employees by department

--- src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -2,7 +2,15 @@ package com.bridgelabz.employeepayrollapp.repository;
 
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeePayrollRepository
         extends JpaRepository<EmployeePayrollModel, Integer> {
+
+    // ---- UC6: Get employees by department ----
+    @Query(value = "SELECT e FROM EmployeePayrollModel e JOIN e.department d WHERE d = :department")
+    List<EmployeePayrollModel> findEmployeesByDepartment(@Param("department") String department);
 }
